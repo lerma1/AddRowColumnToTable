03760d4cbc02848e6d8930ec7f7a636cb7df25fa
lerma1 <lerma1@mail.ru>
2019-10-17 09:43:19 +0700

    update undo-redo

--- src/Components/App.js
+++ src/Components/App.js
@@ -10,15 +10,11 @@ import "../css/style.css"
 class App extends Component {
     constructor(props) {
         super(props)
-        this.onClickButtonInsertRow = this.onClickButtonInsertRow.bind(this);
-        this.onClickButtonInsertCol = this.onClickButtonInsertCol.bind(this);
         this.onChoiceTable = this.onChoiceTable.bind(this);
         this.onClickUnDo = this.onClickUnDo.bind(this);
         this.onClickReDo = this.onClickReDo.bind(this);
         this.onClickCellInsertRow = this.onClickCellInsertRow.bind(this);
         this.onClickCellInsertCol = this.onClickCellInsertCol.bind(this);
-        //this.insertRow = this.insertRow.bind(this);
-       // this.insertCol = this.insertCol.bind(this);
 
         this.state = {
             tree: this.props.history.data[0].tree,
@@ -47,9 +43,7 @@ class App extends Component {
 
     insertRow = (value) => {
         const newTree = this.state.tree.addRow(value);
-        let newHistory = {currentIndex: this.state.history.currentIndex + 1, data: this.state.history.data.slice()};
-
-        if (newHistory.currentIndex != newHistory.data.length - 1) newHistory.data.slice(0, newHistory.currentIndex);
+        let newHistory = {currentIndex: this.state.history.currentIndex + 1, data: this.state.history.data.slice(0, this.state.history.currentIndex+1)};
         newHistory.data.push({text: `Вставлена строка в ячейку № ${value}`, tree: newTree});
 
         this.setState({tree: newTree, isCheckedRow: true, isCheckedCol: false, history: newHistory});
@@ -57,9 +51,7 @@ class App extends Component {
 
     insertCol = (value) => {
         const newTree = this.state.tree.addColumn(value);
-        let newHistory = {currentIndex: this.state.history.currentIndex + 1, data: this.state.history.data.slice()};
-
-        if (newHistory.currentIndex != newHistory.data.length - 1) newHistory.data.slice(0, newHistory.currentIndex);
+        let newHistory = {currentIndex: this.state.history.currentIndex + 1, data: this.state.history.data.slice(0, this.state.history.currentIndex+1)};
         newHistory.data.push({text: `Вставлен столбец в ячейку № ${value}`, tree: newTree});
 
         this.setState({tree: newTree, isCheckedRow: false, isCheckedCol: true, history: newHistory});
@@ -75,24 +67,11 @@ class App extends Component {
         this.insertCol(value);
     }
 
-    onClickButtonInsertRow() {
-            const value = Number(document.getElementById("input").value);
-            if(!this.isValidate(value))return;
-            this.insertRow(value);
-
-    }
-
-    onClickButtonInsertCol() {
-        const value = Number(document.getElementById("input").value);
-        if(!this.isValidate(value))return;
-        this.insertCol(value);
-    }
-
     onChoiceTable(value) {
 
         if (value == 1) {
             const newTree = tree1;
-            let newHistory = {currentIndex: this.state.history.currentIndex + 1, data: this.state.history.data.slice()};
+            let newHistory = {currentIndex: this.state.history.currentIndex + 1, data: this.state.history.data.slice(0, this.state.history.currentIndex+1)};
             newHistory.data.push({text: `Создана Таблица №1`, tree: newTree});
 
             this.setState({tree: newTree, history: newHistory, indexCheckedTable: 0});
@@ -100,14 +79,14 @@ class App extends Component {
         }
         if (value == 2) {
             const newTree = tree2;
-            let newHistory = {currentIndex: this.state.history.currentIndex + 1, data: this.state.history.data.slice()};
+            let newHistory = {currentIndex: this.state.history.currentIndex + 1, data: this.state.history.data.slice(0, this.state.history.currentIndex+1)};
             newHistory.data.push({text: `Создана таблица №2`, tree: newTree});
 
             this.setState({history: newHistory, tree: newTree, indexCheckedTable: 1});
         }
         if (value == 3) {
             const newTree = tree3;
-            let newHistory = {currentIndex: this.state.history.currentIndex + 1, data: this.state.history.data.slice()};
+            let newHistory = {currentIndex: this.state.history.currentIndex + 1, data: this.state.history.data.slice(0, this.state.history.currentIndex+1)};
             newHistory.data.push({text: `Создана таблица №3`, tree: newTree});
 
             this.setState({history: newHistory, tree: newTree, indexCheckedTable: 1});
@@ -117,14 +96,14 @@ class App extends Component {
     onClickUnDo() {
 
         const newHistory = {currentIndex: this.state.history.currentIndex - 1, data: this.state.history.data.slice()};
-        this.setState({tree: newHistory.data[this.state.history.currentIndex - 1], history: newHistory});
+        this.setState({tree: newHistory.data[this.state.history.currentIndex - 1].tree, history: newHistory});
         console.log("height", this.state.height);
     }
 
     onClickReDo() {
         if (this.state.history.currentIndex + 1 >= this.state.history.data.length) return;
         const newHistory = {currentIndex: this.state.history.currentIndex + 1, data: this.state.history.data.slice()};
-        this.setState({tree: newHistory.data[this.state.history.currentIndex + 1], history: newHistory});
+        this.setState({tree: newHistory.data[this.state.history.currentIndex + 1].tree, history: newHistory});
 
     }
 
@@ -132,10 +111,7 @@ class App extends Component {
 
     render() {
 
-
         return (
-
-
             <div className="App">
                 <MyNavbar onChoiceTable={this.onChoiceTable}
                           indexCheckedTable={this.state.indexCheckedTable}/>
